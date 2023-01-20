6247f96a6d9f8e5f5e71ded4a534573edea9ddff
ronald <ronald.andrade.02@gmail.com>
2023-01-20 16:27:16 -0300

    Atividade Carros Finalizada

--- src/Atividade_Banco/Correntista.java
+++ src/Atividade_Banco/Correntista.java
@@ -1,11 +1,10 @@
 package Atividade_Banco;
 
 public class Correntista{
-
     private String nome;
     private String cpf;
 
-    public Correntista(String nome, String cpf){
+    public Correntista(String nome, String cpf) {
         this.nome = nome;
         this.cpf = cpf;
     }
@@ -13,8 +12,11 @@ public class Correntista{
     public String getNome(){
         return nome;
     }
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+
     public String getCpf(){
         return cpf;
     }
-
 }
--- src/Atividade_tres_carros/BMW.java
+++ src/Atividade_tres_carros/BMW.java
@@ -1,5 +1,33 @@
 package Atividade_tres_carros;
 
-public class BMW {
+public class BMW extends Carro{
+
+    private final int potencia = 70;
+
+    public BMW(int velocidade_maxima) {
+        super(velocidade_maxima);
+    }
+
+    @Override
+    public Void acelerar() {
+        if (getVelocidade() >= getVelocidadeMaxima()){
+            System.out.println("Velocidade Máxima Alcançada");
+            System.out.println("Velocidade BMW = "+ getVelocidade());
+            return null;
+        }
+        this.setVelocidade(potencia);
+        return null;
+    }
     
+    @Override
+    public Void acelerar(int n) {
+        if (getVelocidade() >= getVelocidadeMaxima()){
+            System.out.println("Velocidade Máxima Alcançada");
+            System.out.println("Velocidade BMW = "+ getVelocidade());
+            return null;
+        }
+        this.setVelocidade(potencia * n);
+        return null;
+    }
+
 }
