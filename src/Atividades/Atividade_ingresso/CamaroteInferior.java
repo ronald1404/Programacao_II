5e8ad9c59c9b88ee814231a36752a7d088a4e953
Krumi <krumi@Mac-mini-de-Raoni-5.local>
2023-01-20 20:04:10 -0300

    Override nos Camarotes*.java

--- src/Atividades/Atividade_ingresso/CamaroteInferior.java
+++ src/Atividades/Atividade_ingresso/CamaroteInferior.java
@@ -1,14 +1,19 @@
 package Atividades.Atividade_ingresso;
 
-public class CamaroteInferior extends Vip{
+public class CamaroteInferior extends Vip {
     private String local;
 
     public String getLocal() {
         return local;
     }
 
-    public void exibirLocal(){
+    public void exibirLocal() {
         System.out.println("O local do ingresso é: " + getLocal());
     }
-    
+
+    @Override
+    public void imprime() {
+        super.imprime();
+        System.out.println("Camarote Inferior");
+    }
 }
--- src/Atividades/Atividade_ingresso/CamaroteSuperior.java
+++ src/Atividades/Atividade_ingresso/CamaroteSuperior.java
@@ -1,11 +1,18 @@
 package Atividades.Atividade_ingresso;
 
-public class CamaroteSuperior extends Vip{
+public class CamaroteSuperior extends Vip {
 
     public double valorExtra = 10;
 
     @Override
     public double getValor() {
-        return super.getValor()+valorExtra;
+        return super.getValor() + valorExtra;
+    }
+
+    @Override
+    public void imprime() {
+        super.imprime();
+        System.out.println("Camarote Superior");
+
     }
 }
