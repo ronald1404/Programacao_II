8d2ac09ee58c614727f65b09c551b108fbf4a910
ronald <ronald.andrade.02@gmail.com>
2023-01-26 16:56:54 -0300

    polimorfismo exemplo

--- src/Atividades/Atividade05/Main.java
+++ src/Atividades/Atividade05/Main.java
@@ -1,7 +1,10 @@
 package Atividades.Atividade05;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        InterfaceCarro carro;
 
         BMW bmw = new BMW(50);
 
@@ -21,5 +24,20 @@ public class Main {
         System.out.println("\nVelocidade atual do Fusca = "+ bmw.getVelocidade());
 
         fusca.calcularVelocidade(1000, 30);
+
+        System.out.println("Digite 1 ou 2");
+        int aux = new Scanner(System.in).nextInt();
+
+        // exemplo de polimorfismo onde um objeto pode assumir outras formas
+        
+        if (aux == 1){
+            carro = bmw;
+        }
+        else 
+            carro = fusca;
+
+        System.out.println(((ModeloCarro) carro).getVelocidade());
+
+
     }
 }
